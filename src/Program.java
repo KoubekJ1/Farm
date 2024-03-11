b063291cbd270761c15ed1ccb54e836c5cf867d4
koubek <koubek.jenda@icloud.com>
2024-03-11 09:06:08 +0100

    added runProgram() method, created manageFlowers() and manageAnimals() without implementation

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,23 @@
+import util.Input;
+
 public class Program {
+
+    private static int answer;
+
     public static void runProgram() {
+        System.out.println("Welcome!");
+        answer = Input.question(new String[]{"Manage flowers", "Manage animals"});
+        switch (answer) {
+            case 1 -> manageFlowers();
+            case 2 -> manageAnimals();
+        }
+    }
+
+    public static void manageFlowers() {
+
+    }
+
+    public static void manageAnimals() {
 
     }
 }
