de2d2fe9d893d110cac33d4ea35af8c7f2ea978e
koubek <koubek.jenda@icloud.com>
2024-03-11 08:56:06 +0100

    added getInt() method

--- src/util/Input.java
+++ src/util/Input.java
@@ -4,4 +4,14 @@ import java.util.Scanner;
 
 public abstract class Input {
     private static Scanner scanner = new Scanner(System.in);
+
+    public static int getInt() {
+        while (true) {
+            try {
+                return scanner.nextInt();
+            } catch (NumberFormatException ignored) {
+
+            }
+        }
+    }
 }
