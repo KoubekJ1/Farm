4978c32307eb5920c23c50fa2c09fda50456b1d7
koubek <koubek.jenda@icloud.com>
2024-03-11 08:12:53 +0100

    added method for adding flowers

--- src/Farm.java
+++ src/Farm.java
@@ -7,4 +7,16 @@ public class Farm {
     public Farm() {
         flowers = new ArrayList<>();
     }
+
+    public void addFlower(Flower flower) {
+        ArrayList<FlowerType> flowerTypes = new ArrayList<>();
+        for (Flower flower1 : flowers) {
+            if (!flowerTypes.contains(flower1.getType())) {
+                flowerTypes.add(flower1.getType());
+            }
+        }
+        if (flowerTypes.size() < 5 || flowerTypes.contains(flower.getType())) {
+            flowers.add(flower);
+        }
+    }
 }
