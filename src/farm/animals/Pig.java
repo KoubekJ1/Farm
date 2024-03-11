465589d9eefd5bbb3cf958cfae6bfbc08c101c46
koubek <koubek.jenda@icloud.com>
2024-03-11 08:40:47 +0100

    added Pig class

---
+++ src/farm/animals/Pig.java
@@ -0,0 +1,13 @@
+package farm.animals;
+
+public class Pig extends Animal {
+
+    public Pig(String name) {
+        super("Pig", name, Size.BIG, "Bath regularly");
+    }
+
+    @Override
+    public void pet() {
+        System.out.println("Chro chro");
+    }
+}
